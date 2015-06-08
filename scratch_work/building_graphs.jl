52026cbbe01f234e749d060e9a5063181857e3d8
John Spaw <jspaw@millikin.edu>
2015-06-08 15:27:32 -0500

    Tested basic LightGraphs and GraphLayout functionality

--- scratch_work/building_graphs.jl
+++ scratch_work/building_graphs.jl
@@ -20,8 +20,28 @@ setNode!(e4,[n1,n5]);
 test_net=HybridNetwork([n1,n2,n5],[e1,e4]);
 
 printNodes(test_net)
+
+for i=1:2
+    println(" ")
+end
+
 printEdges(test_net)
 
+for i=1:2
+    println(" ")
+end
 
+using LightGraphs
+g = LightGraphs.Graph(5)
+add_edge!(g,1,2)
+add_edge!(g,1,3)
+
+am = full(adjacency_matrix(g))
+
+println(am)
+
+using GraphLayout
+loc_x, loc_y = GraphLayout.layout_spring_adj(am)
+draw_layout_adj(am, loc_x, loc_y, filename = "Testgraph.svg")
 
 
