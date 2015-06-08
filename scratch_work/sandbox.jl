aa327bcfa52c0216f02e590bd81a2fe22777aca2
John Spaw <jspaw@millikin.edu>
2015-06-08 15:54:36 -0500

    Created sandbox file for testing of various code snippets

---
+++ scratch_work/sandbox.jl
@@ -0,0 +1,18 @@
+#Scratch file (sandbox
+#Testing miscellaneous code snippets
+#John Spaw 6-8-15
+
+println( )
+println("*****************")
+println("Testing a basic for loop... list all items in [1,4,7,8]")
+println( )
+
+a = [1,4,7,8]
+for i in a
+	println(i)
+end
+
+println( )
+println("*****************"+"dog")
+println("Testing a basic for loop... list all items in [1,4,7,8]")
+println( )
