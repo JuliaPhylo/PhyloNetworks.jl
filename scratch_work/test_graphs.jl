738d833c9b09ab1541ae53b7c73ddb91ac6f8838
John Spaw <jspaw@millikin.edu>
2015-06-09 15:35:34 -0500

    Created test_graphs file to easily store CF graphs that can be called as examples for testing Each example is stored as a function that returns the graph.

---
+++ scratch_work/test_graphs.jl
@@ -0,0 +1,34 @@
+#Graphs for testing algorithms
+#John Spaw 6-9-15
+#Contains numerous examples of CF trees and networks used for testing algorithms
+################################################################################
+
+
+#####################################################
+#Simple tree with 4 nodes, 3 edges, 0 hybrid nodes
+#####################################################
+function create_g1()
+	#Edges
+	e1 = Edge(1,5.0)
+	e2 = Edge(2,1.0)
+	e3 = Edge(3,7.0)
+	println("Edges created")
+	
+	#Nodes
+	n1 = Node(1, false, false, [e1, e2]);
+	n2 = Node(2, true, false, [e1]);
+	n3 = Node(3, false, false, [e2, e3]);
+	n4 = Node(4, true, false, [e3]);
+	println("Nodes created")
+
+	#Nodes --> edges
+	setNode!(e1,[n1,n2])
+	setNode!(e2,[n1,n3])
+	setNode!(e3,[n3,n4])
+	
+	g1=HybridNetwork([n1,n2,n3,n4],[e1,e2,e3])
+	println("CF network created")
+	
+	return g1
+end
+#####################################################
