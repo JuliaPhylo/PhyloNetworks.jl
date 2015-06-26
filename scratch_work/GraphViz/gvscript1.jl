84bdacc3295409ace18ed04b2a39c30c36d4a4ff
JohnPSpaw <jpspaw@gmail.com>
2015-06-26 00:34:15 -0500

    Revert "Scratch"

--- scratch_work/GraphViz/gvscript1.jl
+++
@@ -1,4 +0,0 @@
-using GraphViz
-g = open("graph.dot","r") do io Graph(io) end
-GraphViz.layout!(g)
-GraphViz.render_x11(GraphViz.Context(),g)
--- startupscript.jl
+++
@@ -1,8 +0,0 @@
-#John Spaw
-#This script includes all of the important base files for starting a Julia session
-
-include("types.jl")
-include("functions.jl")
-include("scratch_work/Misc/test_graphs.jl")
-include("visualization/dotexport.jl")
-
