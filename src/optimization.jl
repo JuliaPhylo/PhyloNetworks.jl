7210e78e8b9e7e7b5a7227a6170925ba52ebe361
Claudia Solis-Lemus <claudiar.solislemus@gmail.com>
2015-09-29 18:25:41 -0500

    strange behavior with variable names, modified inside snaq

--- src/optimization.jl
+++ src/optimization.jl
@@ -1360,8 +1360,8 @@ optTopRun1!(currT::HybridNetwork, d::DataCF, hmax::Int64) = optTopRun1!(currT, m
 # it differs from optTopRuns in that it creates a deepcopy of the starting topology
 # only currT and d are necessary, all others are optional and have default values
 function snaq(currT0::HybridNetwork, d::DataCF; hmax=1::Int64, M=multiplier::Number, Nfail=numFails::Int64,ftolRel=fRel::Float64, ftolAbs=fAbs::Float64, xtolRel=xRel::Float64, xtolAbs=xAbs::Float64, verbose=false::Bool, closeN=true::Bool, Nmov0=numMoves::Vector{Int64}, runs=10::Int64, outgroup="none"::String, rootname="snaq"::String, returnNet=true::Bool, seed=0::Int64, probST=0.3::Float64)
-    currT=deepcopy(currT0)
-    optTopRuns!(currT0, M, Nfail, d, hmax,ftolRel, ftolAbs, xtolRel, xtolAbs, verbose, closeN, Nmov0, runs, outgroup, rootname, returnNet,seed,probST)
+    startnet=deepcopy(currT0)
+    optTopRuns!(startnet, M, Nfail, d, hmax,ftolRel, ftolAbs, xtolRel, xtolAbs, verbose, closeN, Nmov0, runs, outgroup, rootname, returnNet,seed,probST)
 end
 
 
