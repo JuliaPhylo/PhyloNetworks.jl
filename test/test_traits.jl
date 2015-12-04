2ccd1e88970dac187ae3d32309d31bc0cbafb5e2
Claudia Solis-Lemus <claudiar.solislemus@gmail.com>
2015-12-03 18:39:14 -0600

    test traits

--- test/test_traits.jl
+++ test/test_traits.jl
@@ -1,18 +1,53 @@
  # test for trait evolution
  # Claudia November 2015
 
+#using PhyloNetworks
+
 include("../src/types.jl")
 include("../src/functions.jl")
 include("../src/traits.jl")
 
-#tree = "(A,(B,(C,D)));"
-tree= "(A,((B,#H1),(C,(D)#H1)));" #fails
-#net=readTopology(tree)
+tree= "(A,((B,#H1),(C,(D)#H1)));"
+
 net=readTopologyLevel1(tree)
 printEdges(net)
-#[e.isChild1 for e in net.edge]
+
+## Re-root the tree so that it matches my example
+root!(net, "A")
+printEdges(net)
+
+## Preorder
 directEdges!(net)
-#[e.isChild1 for e in net.edge]
 preorder!(net)
 
-sharedPathMatrix(net)
+## V matrix
+V1 = sharedPathMatrix(net)
+
+## By hand V matrix
+l = ones(1, 9)
+l[1] = 0.5; l[9] = 0.5
+gamma = ones(1, 9); gamma[3] = 0.1; gamma[7] = 0.9
+V2 = [0 0 0 0 0 0 0 0 0;
+    0 l[1] 0 0 0 0 0 0 0;
+    0 0 l[9] l[9] l[9] l[9] l[9] l[9] l[9];
+    0 0 0 l[9]+l[4] l[9] l[9]+l[4] l[9]+gamma[3]*l[4] l[9] l[9]+gamma[3]*l[4];
+    0 0 0 0 l[9]+l[8] l[9] l[9]+gamma[7]*l[8] l[9]+l[8] l[9]+gamma[7]*l[8];
+    0 0 0 0 0 l[9]+l[4]+l[2] l[9]+gamma[3]l[4] l[9] l[9]+gamma[3]l[4];
+    0 0 0 0 0 0 l[9]+gamma[3]^2*(l[3]+l[4])+gamma[7]^2*(l[7]+l[8]) l[9]+gamma[7]*l[8] l[9]+gamma[3]^2*(l[3]+l[4])+gamma[7]^2*(l[7]+l[8]);
+    0 0 0 0 0 0 0 l[9]+l[8]+l[5] l[9]+gamma[7]*l[8];
+    0 0 0 0 0 0 0 0 l[9]+gamma[3]^2*(l[3]+l[4])+gamma[7]^2*(l[7]+l[8])+l[6]]
+
+for i in 1:9
+    for j in 1:9
+        if i > j
+            V2[i,j] = V2[j,i]
+        end
+    end
+end
+
+## Seem identical
+V1
+V2
+
+## Return false (floating errors ?)
+V1 == V2
