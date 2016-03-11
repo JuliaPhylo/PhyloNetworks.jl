319633c16ba6782e398c138dd606ceb8cc3842de
Claudia Solis-Lemus <claudiar.solislemus@gmail.com>
2016-03-11 12:47:50 -0600

    removed DEBUG

--- src/functions.jl
+++ src/functions.jl
@@ -15,7 +15,7 @@ using NLopt # for branch lengths optimization
 
 import Base.show
 
-const DEBUG = true
+const DEBUG = false
 const DEBUGC = false #more detailed prints
 const CHECKNET = false #for debugging only
 const REDIRECT = false # for debugging to a file later
