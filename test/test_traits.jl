8fad972cd4b55151971d226f0330c25803870a50
pbastide <paul.bastide@gmail.com>
2017-01-19 16:35:14 +0100

    Test formula 1.

--- test/test_traits.jl
+++ test/test_traits.jl
@@ -45,6 +45,52 @@ test_show(V2)
 
 @test_approx_eq V1[:All] V2
 
+## New formula
+net = readTopology("(((Ag:5,(#H1:1::0.056,((Ak:2,(E:1,#H2:1::0.004):1):1,(M:2)#H2:1::0.996):1):1):1,(((((Az:1,Ag2:1):1,As:2):1)#H1:1::0.944,Ap:4):1,Ar:5):1):1,(P:4,20:4):3,165:7);")
+preorder!(net)
+V1 = sharedPathMatrix(net)
+
+hyb = net.hybrid[2]
+
+## Find child edge
+hyb_edges = [e.hybrid for e in hyb.edge]
+child_edge = hyb.edge[!hyb_edges][1]
+child = child_edge.isChild1 ? child_edge.node[1] : child_edge[17].node[2]
+## Find number of child edge
+nodeNumbersTopOrder = [n.number for n in net.nodes_changed]
+p = indexin([child.number], nodeNumbersTopOrder)
+## Find parents edges and node numbers
+par_edge_1 = hyb.edge[hyb_edges][1]
+a_n = PhyloNetworks.getOtherNode(par_edge_1, child)
+a = indexin([a_n.number], nodeNumbersTopOrder)
+par_edge_2 = hyb.edge[hyb_edges][2]
+b_n = PhyloNetworks.getOtherNode(par_edge_2, child)
+b = indexin([b_n.number], nodeNumbersTopOrder)
+
+## Gamma value
+gam = par_edge_1.gamma
+
+## Two extreme networks
+par_edge_1.gamma = 1
+par_edge_2.gamma = 0
+V_t_1 = sharedPathMatrix(net)
+
+par_edge_1.gamma = 0
+par_edge_2.gamma = 1
+V_t_2 = sharedPathMatrix(net)
+
+## Descendant indicatrice matrix
+des = PhyloNetworks.descendants(par_edge_1)
+mask = indexin(des, V_t_1.nodeNumbersTopOrder)
+D = zeros(net.numNodes, net.numNodes)
+D[mask, mask] = 1.0
+
+## Formula
+V2 = gam*V_t_1[:All] + (1-gam)*V_t_2[:All] - gam*(1-gam) * (V_t_1.V[p, p] - V_t_1.V[a, b] + V_t_2.V[p, p] - V_t_2.V[a, b]) .* D
+
+@test_approx_eq V1[:All] V2
+
+
 # ## Seem identical
 # V1[:All]
 # V2
