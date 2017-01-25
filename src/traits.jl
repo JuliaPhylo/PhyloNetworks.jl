b850564c2310ff7575468b69ac45731eb53b33e2
pbastide <paul.bastide@gmail.com>
2017-01-25 10:47:19 +0100

    Allow for a fixed value for lambda.

--- src/traits.jl
+++ src/traits.jl
@@ -644,7 +644,8 @@ function phyloNetworklm(X::Matrix,
                         msng=trues(length(Y))::BitArray{1}, # Which tips are not missing ?
                         model="BM"::AbstractString,
                         ind=[0]::Vector{Int},
-                        startingValue=0.5::Real)
+                        startingValue=0.5::Real,
+                        fixedValue=Nullable{Real}()::Nullable{Real})
     # Geting variance covariance
     V = sharedPathMatrix(net)
     # Get gammas and heights
@@ -652,7 +653,8 @@ function phyloNetworklm(X::Matrix,
     times = getHeights(net)
     # Fit
     phyloNetworklm(X, Y, V, gammas, times;
-                   msng = msng, model=model, ind=ind, startingValue=startingValue)
+                   msng = msng, model=model, ind=ind,
+                   startingValue=startingValue, fixedValue=fixedValue)
 end
 
 # Same function, but when the matrix V is already known.
@@ -664,7 +666,8 @@ function phyloNetworklm(X::Matrix,
                         msng=trues(length(Y))::BitArray{1}, # Which tips are not missing ?
                         model="BM"::AbstractString,
                         ind=[0]::Vector{Int},
-                        startingValue=0.5::Real)
+                        startingValue=0.5::Real,
+                        fixedValue=Nullable{Real}()::Nullable{Real})
     ## Choose Model
     if (model == "BM")
         return phyloNetworklm_BM(X, Y, V;
@@ -672,7 +675,8 @@ function phyloNetworklm(X::Matrix,
     end
     if (model == "lambda")
         return phyloNetworklm_lambda(X, Y, V, gammas, times;
-                                     msng=msng, ind=ind, startingValue=startingValue) 
+                                     msng=msng, ind=ind,
+                                     startingValue=startingValue, fixedValue=fixedValue) 
     end
 end
 
@@ -790,32 +794,38 @@ function phyloNetworklm_lambda(X::Matrix,
                                xtolRel=xRelTr::AbstractFloat,
                                ftolAbs=fAbsTr::AbstractFloat,
                                xtolAbs=xAbsTr::AbstractFloat,
-                               startingValue=0.5::Real)
-    # Find Best lambda using optimize from package NLopt
-    opt = NLopt.Opt(:LN_BOBYQA, 1)
-    NLopt.ftol_rel!(opt, ftolRel) # relative criterion
-    NLopt.ftol_abs!(opt, ftolAbs) # absolute critetion 
-    NLopt.xtol_rel!(opt, xtolRel) # criterion on parameter value changes
-    NLopt.xtol_abs!(opt, xtolAbs) # criterion on parameter value changes
-    NLopt.maxeval!(opt, 1000) # max number of iterations
-    NLopt.lower_bounds!(opt, 1e-100) # Lower bound  
-    # Upper Bound
-    up = maxLambda(times, V)
-    NLopt.upper_bounds!(opt, up-up/1000)
-    count = 0
-    function fun(x::Vector{Float64}, g::Vector{Float64})
-        x = convert(AbstractFloat, x[1])
-        res = logLik_lam(x, X, Y, V, gammas, times; msng=msng, ind=ind)
-        count =+ 1
-        #println("f_$count: $(round(res,5)), x: $(x)")
-        return res
+                               startingValue=0.5::Real,
+                               fixedValue=Nullable{Real}()::Nullable{Real})
+    if isnull(fixedValue)
+        # Find Best lambda using optimize from package NLopt
+        opt = NLopt.Opt(:LN_BOBYQA, 1)
+        NLopt.ftol_rel!(opt, ftolRel) # relative criterion
+        NLopt.ftol_abs!(opt, ftolAbs) # absolute critetion 
+        NLopt.xtol_rel!(opt, xtolRel) # criterion on parameter value changes
+        NLopt.xtol_abs!(opt, xtolAbs) # criterion on parameter value changes
+        NLopt.maxeval!(opt, 1000) # max number of iterations
+        NLopt.lower_bounds!(opt, 1e-100) # Lower bound  
+        # Upper Bound
+        up = maxLambda(times, V)
+        NLopt.upper_bounds!(opt, up-up/1000)
+        count = 0
+        function fun(x::Vector{Float64}, g::Vector{Float64})
+            x = convert(AbstractFloat, x[1])
+            res = logLik_lam(x, X, Y, V, gammas, times; msng=msng, ind=ind)
+            count =+ 1
+            #println("f_$count: $(round(res,5)), x: $(x)")
+            return res
+        end
+        NLopt.min_objective!(opt, fun)
+        fmin, xmin, ret = NLopt.optimize(opt, [startingValue])
+        # Best value dans result
+        res_lam = xmin[1]
+    else
+        res_lam = fixedValue
     end
-    NLopt.min_objective!(opt, fun)
-    fmin, xmin, ret = NLopt.optimize(opt, [startingValue])
-    # Best value dans result
-    transform_matrix_lambda!(V, xmin[1], gammas, times)
+    transform_matrix_lambda!(V, res_lam, gammas, times)
     res = phyloNetworklm_BM(X, Y, V; msng=msng, ind=ind)
-    res.lambda = xmin[1]
+    res.lambda = res_lam
     res.model = "lambda"
     return res
 end
@@ -988,7 +998,8 @@ function phyloNetworklm(f::Formula,
                         xtolRel=xRelTr::AbstractFloat,
                         ftolAbs=fAbsTr::AbstractFloat,
                         xtolAbs=xAbsTr::AbstractFloat,
-                        startingValue=0.5::Real)
+                        startingValue=0.5::Real,
+                        fixedValue=Nullable{Real}()::Nullable{Real})
     # Match the tips names: make sure that the data provided by the user will
     # be in the same order as the ordered tips in matrix V.
     V = sharedPathMatrix(net)
@@ -1034,7 +1045,8 @@ function phyloNetworklm(f::Formula,
     Y = convert(Vector{Float64},DataFrames.model_response(mf))
     # Fit the model (Method copied from DataFrame/src/statsmodels/statsmodels.jl, lines 47-58)
     DataFrames.DataFrameRegressionModel(phyloNetworklm(mm.m, Y, V, gammas, times;
-                                                       msng=mf.msng, model=model, ind=ind, startingValue=startingValue), mf, mm)
+                                                       msng=mf.msng, model=model, ind=ind,
+                                                       startingValue=startingValue, fixedValue=fixedValue), mf, mm)
     #    # Create the object
     #    phyloNetworkLinPredModel(DataFrames.DataFrameRegressionModel(fit, mf, mm),
     #    fit.V, fit.Vy, fit.RL, fit.Y, fit.X, fit.logdetVy, ind, mf.msng)
