051645622faf646da6ed72eab4ce0338a117df4c
Paul Bastide <paul.bastide@gmail.com>
2017-03-31 15:44:45 +0200

    Change structure of make_weave.jl for better legibility.

--- docs/src/man/src/make_weave.jl
+++ docs/src/man/src/make_weave.jl
@@ -2,9 +2,13 @@ using Weave, Fontconfig, Cairo
 
 set_chunk_defaults(Dict{Symbol, Any}(:results => "hidden", :eval => false))
 
-weave(Pkg.dir("PhyloNetworks","docs","src", "man", "src","trait_tree.jmd"),
-      informat="markdown",
-      out_path = Pkg.dir("PhyloNetworks","docs","src", "man"),
-      fig_path = "../assets/figures",
-      fig_ext = ".png",
-      doctype = "github")
+files_to_weave = ["trait_tree.jmd"]
+
+for file in files_to_weave
+    weave(Pkg.dir("PhyloNetworks","docs","src", "man", "src", file),
+        informat = "markdown",
+        out_path = Pkg.dir("PhyloNetworks","docs","src", "man"),
+        fig_path = "../assets/figures",
+        fig_ext  = ".png",
+        doctype  = "github")
+end
