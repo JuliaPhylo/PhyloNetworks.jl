35db921ecfaaf8288a58193429e18f8125f85448
Paul Bastide <paul.bastide@gmail.com>
2017-08-01 16:34:24 +0200

    Go back to osx. draw_fig to true.

--- docs/make.jl
+++ docs/make.jl
@@ -9,7 +9,7 @@ deploydocs(
     deps   = Deps.pip("pygments", "mkdocs", "mkdocs-material", "python-markdown-math"),
     repo = "github.com/pbastide/PhyloNetworks.jl.git",
     julia  = "0.6",
-#    osname = "osx",
-    osname = "linux",
+    osname = "osx",
+#    osname = "linux",
     latest = "weave_doc"
 )
