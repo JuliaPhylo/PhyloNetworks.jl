31764da008cc4df6743949df623f523db942c0ba
Paul Bastide <paul.bastide@gmail.com>
2017-09-07 14:26:44 +0200

    Create make.sh script to build documentation only on linux.

---
+++ docs/make.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Indea of this script taken from: http://steven.casagrande.io/articles/travis-ci-and-if-statements/
+
+set -ev
+
+if [ "$TRAVIS_OS_NAME" == "linux" ]; then
+    - julia -e 'Pkg.clone("https://github.com/pbastide/Documenter.jl")'
+    # - julia -e 'Pkg.checkout("Documenter", "release-0.11")'
+    - julia -e 'Pkg.add("Cairo")'
+    - julia -e 'Pkg.add("Fontconfig")'
+    - rm $HOME/.julia/lib/v$TRAVIS_JULIA_VERSION/Compose.ji
+    - julia -e 'Pkg.add("Weave")'
+    - julia -e 'cd(Pkg.dir("PhyloNetworks")); include(joinpath("docs", "make.jl"))'
+fi
+
+exit 0;
