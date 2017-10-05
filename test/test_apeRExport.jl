c0bc465a4bba20e0806a6ec865ca1ed50b6fd05b
William Sparks <kwsparks@wisc.edu>
2017-10-05 17:19:28 -0500

    removed apeRExport and sexp test where branch lengths are not defined

--- test/test_apeRExport.jl
+++ test/test_apeRExport.jl
@@ -4,19 +4,11 @@ info("Testing `apeRExport` function")
     #checking for absence of errors, not testing for correctness
 
 #on a tree
-net = readTopology("(A,(B,(C,D)));")
-phy = apeRExport(net)
-R"plot.evonet($phy)"    
-            
 net = readTopology("(A:1.0,(B:1.0,(C:1.0,D:1.0):1.0):1.0);")
 phy = apeRExport(net; useEdgeLength=true)
 R"plot.evonet($phy)"
         
 #on a network
-net = readTopology("(((A,(B)#H1:::0.9),(C,#H1:::0.1)),D);")
-phy = apeRExport(net)
-R"plot.evonet($phy)"    
-        
 net = readTopology("(((A:1.0,(B:1.0)#H1:1.0::0.9):1.0,(C:1.0,#H1:1.0::0.1):1.0):1.0,D:1.0);")
 phy = apeRExport(net; useEdgeLength=true)
 R"plot.evonet($phy)"
@@ -33,19 +25,11 @@ info("Testing `apeRExport` function")
     #checking for absence of errors, not testing for correctness
 
 #on a tree
-net = readTopology("(A,(B,(C,D)));")
-phy = sexp(net)
-R"plot.evonet($phy)"
-        
 net = readTopology("(A:1.0,(B:1.0,(C:1.0,D:1.0):1.0):1.0);")
 phy = sexp(net)
 R"plot.evonet($phy)"
 
 #on a network
-net = readTopology("(((A,(B)#H1:::0.9),(C,#H1:::0.1)),D);")
-phy = sexp(net)
-R"plot.evonet($phy)"
-        
 net = readTopology("(((A:1.0,(B:1.0)#H1:1.0::0.9):1.0,(C:1.0,#H1:1.0::0.1):1.0):1.0,D:1.0);")
 phy = sexp(net)
 R"plot.evonet($phy)"
