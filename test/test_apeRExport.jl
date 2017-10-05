1e707dd5a5848dfce41af2d9abe0ea920ce76544
William Sparks <kwsparks@wisc.edu>
2017-10-05 16:34:45 -0500

    modified  to test for errorless return of apeRExport and sexp plotting functions

--- test/test_apeRExport.jl
+++ test/test_apeRExport.jl
@@ -1,33 +1,55 @@
-@testset "Testing `apeRExport` function" begin
-
 R"library(ape)"
+
+@testset "Testing `apeRExport` function" begin
+    #checking for absence of errors, not testing for correctness
 
 #on a tree
 net = readTopology("(A,(B,(C,D)));")
 phy = apeRExport(net)
-@test R"plot.evonet($phy)"
-
-
-    
+R"plot.evonet($phy)"    
+            
+net = readTopology("(A:1.0,(B:1.0,(C:1.0,D:1.0):1.0):1.0);")
+phy = apeRExport(net; useEdgeLength=true)
+R"plot.evonet($phy)"
+        
 #on a network
 net = readTopology("(((A,(B)#H1:::0.9),(C,#H1:::0.1)),D);")
 phy = apeRExport(net)
-@test R"plot.evonet($phy)"
+R"plot.evonet($phy)"    
+        
+net = readTopology("(((A:1.0,(B:1.0)#H1:1.0::0.9):1.0,(C:1.0,#H1:1.0::0.1):1.0):1.0,D:1.0);")
+phy = apeRExport(net; useEdgeLength=true)
+R"plot.evonet($phy)"
+
+net = readTopology("(((A:1.0,(B:1.0)#H1:1.0::0.9):1.0,(C:1.0,#H1:1.0::0.1):1.0):1.0,D:1.0);")
+phy = apeRExport(net; mainTree=true)
+R"plot.evonet($phy)"
+
+net = readTopology("(((A:1.0,(B:1.0)#H1:1.0::0.9):1.0,(C:1.0,#H1:1.0::0.1):1.0):1.0,D:1.0);")
+phy = apeRExport(net; mainTree=true, useEdgeLength=true)
+R"plot.evonet($phy)"
 
 end
 
 @testset "Testing `sexp` function on `HybridNetwork` objects" begin
-
-R"library(ape)"
+    #checking for absence of errors, not testing for correctness
 
 #on a tree
 net = readTopology("(A,(B,(C,D)));")
 phy = sexp(net)
-@test R"plot.evonet($phy)"
+R"plot.evonet($phy)"
+        
+net = readTopology("(A:1.0,(B:1.0,(C:1.0,D:1.0):1.0):1.0);")
+phy = sexp(net)
+R"plot.evonet($phy)"
 
-    
 #on a network
 net = readTopology("(((A,(B)#H1:::0.9),(C,#H1:::0.1)),D);")
 phy = sexp(net)
-@test R"plot.evonet($phy)"
+R"plot.evonet($phy)"
+        
+net = readTopology("(((A:1.0,(B:1.0)#H1:1.0::0.9):1.0,(C:1.0,#H1:1.0::0.1):1.0):1.0,D:1.0);")
+phy = sexp(net)
+R"plot.evonet($phy)"
+        
 end
