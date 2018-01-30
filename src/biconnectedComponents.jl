31ed4473f0cefa2c74c3abd9eefb1276ccfe3023
Cecile Ane <cecileane@users.noreply.github.com>
2018-01-30 15:26:10 -0600

    tarjan algo to get blobs

--- src/biconnectedComponents.jl
+++ src/biconnectedComponents.jl
@@ -1,68 +1,107 @@
 """
     biconnectedComponents(network, ignoreTrivial=false)
 
-Calculate biconnected components -aka "blobs, using Tarjan's algorithm:
-output is a array of arrays of edges. options:
+Calculate biconnected components (aka "blobs) using Tarjan's algorithm:
+the output is an array of arrays of edges.
+If `ignoreTrivial` is true, trivial components (of a single edge)
+are not returned.
 
-- ignoreTrivial: if true, trivial components are not returned
+**Warnings**: for nodes, fields `k`, `inCycle`, and `prev``
+are modified during the algorithm. They are used to store the
+node's "index" (time of visitation), "lowpoint", and the node's
+"parent", as defined by the order in which nodes are visited.
 
-**warnings**: for nodes, fields `k`, `inCycle` and `isExtBadTriangle`
-are modified during the algorithm.
-They are used to store "depth", "lowpoint" and "onStack".
+References:
+- p. 153 of [Tarjan (1972)](http://epubs.siam.org/doi/pdf/10.1137/0201010).
+  Depth first search and linear graph algorithms,
+  SIAM Journal on Computing, 1(2):146-160
+- on [geeksforgeeks](https://www.geeksforgeeks.org/biconnected-components/),
+  there is an error (as of 2018-01-30):
+  `elif v != parent[u] and low[u] > disc[v]:` (python version)
+  should be replaced by
+  `elif v != parent[u] and disc[u] > disc[v]:`
+- nice explanation
+  [here](https://www.cs.cmu.edu/~avrim/451f12/lectures/biconnected.pdf)
 """
 function biconnectedComponents(net, ignoreTrivial=false::Bool)
     for n in net.node
         n.inCycle = -1 # -1 for missing, node not visited yet
-        n.k = -1
-        n.isExtBadTriangle = false
+        n.k = -1       # inCycle = lowpoint
+        n.prev = nothing # parent: will be a node later
     end
-    S = Vector{Node}(0) # temporary stack
-    blobs = Vector{Vector{Node}}(0) # will contain the blobs
+    S = Vector{Edge}(0) # temporary stack
+    blobs = Vector{Vector{Edge}}(0) # will contain the blobs
     biconnectedComponents(net.node[net.root], [0], S, blobs, ignoreTrivial)
+    # if stack not empty: create last connected component
+    if length(S)>0
+        #println("stack not empty at the end.")
+        bb = Vector{Edge}(0)
+        while length(S)>0
+            e2 = pop!(S)
+            push!(bb, e2)
+        end
+        if !ignoreTrivial || length(bb)>1
+            push!(blobs, bb)
+        end
+    end
     return blobs
 end
 
 """
 when applied to a node (not a network), index is an array
-containing a single integer --thus mutable. order in which
-nodes are found.
+containing a single integer --thus mutable: order in which
+nodes are visited.
 """
 function biconnectedComponents(node, index, S, blobs, ignoreTrivial)
-    print("\nentering biconnect on: ")
-    @show node
-    @show index
+    #println("\nentering biconnect, index=$(index[1])")
+    children = 0
     # set depth index for v to the smallest unused index
-    node.k = index[1]       # index
+    node.k = index[1]       # index / disc
     node.inCycle = index[1] # lowpoint / lowlink
-    println("just updated: index $(node.k), lowlink$(node.inCycle)")
+    #println(" for node $(node.number): k=$(node.k), low=$(node.inCycle)")
     index[1] += 1
-    push!(S, node)
-    @show [n.number for n in S]
-    node.isExtBadTriangle = true # is on stack
+    #print(" stack: "); @show [e.number for e in S]
 
     for e in node.edge # each (v, w) in E do
         w = (e.node[1] == node) ? e.node[2] : e.node[1]
+        #println(" w: $(w.number) along edge $(e.number)")
         if w.k == -1 # w not yet visited
+            w.prev = node # set parent of w to current node
+            #println(" w's parent = $(w.prev.number)")
+            children += 1
+            push!(S, e)
+            #println(" edge $(e.number) on stack, intermediate step for node=$(node.number)")
             biconnectedComponents(w, index, S, blobs, ignoreTrivial)
-            node.inCycle = min(node.inCycle, w.inCycle)
-        elseif w.isExtBadTriangle # it is on stack: hence in current biCC
-            # if w not on stack: e is cross-edge in DFS tree, ignored
-            # the next line may look odd - but is correct.
-            # says w.index not w.lowlink; that is deliberate, from original paper
+            # check if subtree rooted at w has connection to
+            # one of the ancestors of node
+            # Case 1 -- per Strongly Connected Components Article
+            node.inCycle = min(node.inCycle, w.inCycle) # lowpoint
+            #print(" case 1, low=$(node.inCycle) for node $(node.number); ")
+            #println("low=$(w.inCycle) for w $(w.number)")
+            # if node is an articulation point: pop until e
+            if (node.prev == nothing && children > 1) ||
+               (node.prev != nothing && w.inCycle >= node.k)
+                # node is either root or an articulation point
+                # start a new strongly connected component
+                bb = Vector{Edge}(0)
+                while length(S)>0
+                    e2 = pop!(S)
+                    #println(" popping: edge $(e2.number)")
+                    push!(bb, e2)
+                    e2 != e || break
+                end
+                if !ignoreTrivial || length(bb)>1
+                    push!(blobs, bb)
+                end
+            end
+        elseif w != node.prev && node.k > w.k
+            # e is a back edge, not cross edge. Case 2 in article.
             node.inCycle = min(node.inCycle, w.k)
+            #println(" case 2, node $(node.number): low=$(node.inCycle)")
+            push!(S, e)
+            #println(" edge $(e.number) now on stack, final step for node $(node.number)")
+        else
+            #println(" nothing to do from node $(node.number) to w $(w.number) along edge $(e.number)")
         end
-    end
-
-    # if root node: pop the stack and generate a biCC
-    if (node.inCycle == node.k)
-      # start a new strongly connected component
-      bb = Vector{Node}(0)
-      while length(S)>0
-        w = pop!(S)
-        w.isExtBadTriangle = false
-        push!(bb, w)
-        w != node || break # break out of while loop if w == node
-      end
-      push!(blobs, bb)
     end
 end
--- test/test_parsimony.jl
+++ test/test_parsimony.jl
@@ -31,7 +31,27 @@ score, states = parsimonyDiscrete(net, dat)
 
 end
 
-@testset "Testing Tarjan's algorithm to get biconnected components"
+@testset "Testing Tarjan's biconnected components" begin
 
 net = readTopology("(A,(B,(C,D)));")
 a = biconnectedComponents(net);
+@test [[e.number for e in b] for b in a] == [[2],[3],[4],[5],[6],[1]]
+net = readTopology("(((A,(((C,(D)#H2),(E,#H2)))#H1),(B,#H1)),F);")
+a = biconnectedComponents(net);
+@test [[e.number for e in b] for b in a] == [[1],[2],[3],[6],
+  [8, 7, 4, 5],[9],[12],[14, 13, 10, 11],[16],[15]]
+net = readTopology("(((A,(B)#H1),((C,(E)#H2),#H1)),(D,#H2));")
+a = biconnectedComponents(net);
+@test [[e.number for e in b] for b in a] == [[1],
+  [2],[5],[6],[12],[10, 14, 13, 7, 8, 9, 3, 4, 11]]
+net = readTopology("((((A,(B)#H1),((C,(E)#H2),#H1)),(D,#H2)),(((F)#H3,G),(H,#H3)));")
+a = biconnectedComponents(net);
+@test [[e.number for e in b] for b in a] == [[1],[2],[5],[6],[12],
+  [10, 14, 13, 7, 8, 9, 3, 4, 11],[16],[20],[18],
+  [22, 21, 17, 19],[23],[15]]
+a = biconnectedComponents(net, true);
+@test [[e.number for e in b] for b in a] == [[10, 14, 13, 7, 8, 9, 3, 4, 11],
+  [22, 21, 17, 19]]
+# net = readTopology("((((((((((((((Ae_caudata_Tr275,Ae_caudata_Tr276),Ae_caudata_Tr139))#H1,#H2),(((Ae_umbellulata_Tr266,Ae_umbellulata_Tr257),Ae_umbellulata_Tr268),#H1)),((Ae_comosa_Tr271,Ae_comosa_Tr272),(((Ae_uniaristata_Tr403,Ae_uniaristata_Tr357),Ae_uniaristata_Tr402),Ae_uniaristata_Tr404))),(((Ae_tauschii_Tr352,Ae_tauschii_Tr351),(Ae_tauschii_Tr180,Ae_tauschii_Tr125)),((((((((Ae_longissima_Tr241,Ae_longissima_Tr242),Ae_longissima_Tr355),(Ae_sharonensis_Tr265,Ae_sharonensis_Tr264)),((Ae_bicornis_Tr408,Ae_bicornis_Tr407),Ae_bicornis_Tr406)),((Ae_searsii_Tr164,Ae_searsii_Tr165),Ae_searsii_Tr161)))#H2,#H3),#H4))),(((T_boeoticum_TS8,(T_boeoticum_TS10,T_boeoticum_TS3)),T_boeoticum_TS4),((T_urartu_Tr315,T_urartu_Tr232),(T_urartu_Tr317,T_urartu_Tr309)))),(((((Ae_speltoides_Tr320,Ae_speltoides_Tr323),Ae_speltoides_Tr223),Ae_speltoides_Tr251))#H3,((((Ae_mutica_Tr237,Ae_mutica_Tr329),Ae_mutica_Tr244),Ae_mutica_Tr332))#H4))),Ta_caputMedusae_TB2),S_vavilovii_Tr279),Er_bonaepartis_TB1),H_vulgare_HVens23);")
+# above: parsing error because one hybrid has a hybrid child
+end
