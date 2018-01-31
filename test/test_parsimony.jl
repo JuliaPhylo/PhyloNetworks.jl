c3fc62619fd26113f126b1e7ba14e6a6895dc1ea
celinescornavacca <celinescornavacca@celinescornavacca.local>
2018-01-31 04:43:17 +0100

    Added using dependencies to test_parsimony

--- test/test_parsimony.jl
+++ test/test_parsimony.jl
@@ -1,3 +1,7 @@
+using PhyloNetworks
+using Test
+using DataFrames
+
 @testset "Testing parsimony score & reconstruction" begin
 
 # on a tree:
@@ -55,6 +59,6 @@ a = biconnectedComponents(net, true);
 # net = readTopology("((((((((((((((Ae_caudata_Tr275,Ae_caudata_Tr276),Ae_caudata_Tr139))#H1,#H2),(((Ae_umbellulata_Tr266,Ae_umbellulata_Tr257),Ae_umbellulata_Tr268),#H1)),((Ae_comosa_Tr271,Ae_comosa_Tr272),(((Ae_uniaristata_Tr403,Ae_uniaristata_Tr357),Ae_uniaristata_Tr402),Ae_uniaristata_Tr404))),(((Ae_tauschii_Tr352,Ae_tauschii_Tr351),(Ae_tauschii_Tr180,Ae_tauschii_Tr125)),((((((((Ae_longissima_Tr241,Ae_longissima_Tr242),Ae_longissima_Tr355),(Ae_sharonensis_Tr265,Ae_sharonensis_Tr264)),((Ae_bicornis_Tr408,Ae_bicornis_Tr407),Ae_bicornis_Tr406)),((Ae_searsii_Tr164,Ae_searsii_Tr165),Ae_searsii_Tr161)))#H2,#H3),#H4))),(((T_boeoticum_TS8,(T_boeoticum_TS10,T_boeoticum_TS3)),T_boeoticum_TS4),((T_urartu_Tr315,T_urartu_Tr232),(T_urartu_Tr317,T_urartu_Tr309)))),(((((Ae_speltoides_Tr320,Ae_speltoides_Tr323),Ae_speltoides_Tr223),Ae_speltoides_Tr251))#H3,((((Ae_mutica_Tr237,Ae_mutica_Tr329),Ae_mutica_Tr244),Ae_mutica_Tr332))#H4))),Ta_caputMedusae_TB2),S_vavilovii_Tr279),Er_bonaepartis_TB1),H_vulgare_HVens23);")
 # above: parsing error because one hybrid has a hybrid child
 
-a = blobRoots(net)
+a = PhyloNetworks.blobRoots(net)
 @test [n.number for n in a] == [-5, 3, -8, 6, -10, -3, 9, -14, -12, -11, -2, -2]
 end
