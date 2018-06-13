1cc5258e90d40b3e20f18dd0a8d799facdcc3a65
Paul Bastide <paul.bastide@gmail.com>
2018-06-13 18:56:52 +0200

    Still need to deploy the doc.

--- docs/make.jl
+++ docs/make.jl
@@ -2,11 +2,11 @@ using Documenter, PhyloNetworks
 
 makedocs(doctest=false)
 
-# deploydocs(
-#     deps   = Deps.pip("pygments", "mkdocs", "mkdocs-material", "python-markdown-math"),
-#     repo = "github.com/pbastide/PhyloNetworks.jl.git",
-#     julia  = "0.6",
-# #    osname = "osx",
-#     osname = "linux",
-#     latest = "weave_doc"
-# )
+deploydocs(
+    deps   = Deps.pip("pygments", "mkdocs", "mkdocs-material", "python-markdown-math"),
+    repo = "github.com/pbastide/PhyloNetworks.jl.git",
+    julia  = "0.6",
+#    osname = "osx",
+    osname = "linux",
+    latest = "weave_doc"
+)
