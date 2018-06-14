0c04968ed0892e35f1bf25bd39c60ca0c7bd87fb
Paul Bastide <paul.bastide@gmail.com>
2018-06-14 18:18:06 +0200

    Try changing the syntax.

--- docs/make.sh
+++ docs/make.sh
@@ -3,13 +3,12 @@
 
 set -ev
 
+# Install R, then build the doc.
 if [ "$TRAVIS_OS_NAME" == "linux" ]; then
-    ## Install R
     sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E084DAB9;
     sudo add-apt-repository -y "deb http://cran.rstudio.com/bin/linux/ubuntu $(lsb_release -s -c)/";
     sudo apt-get update -qq -y;
     sudo apt-get install git r-base r-base-dev r-recommended -y;
-    ## Build the doc 
     julia -e 'Pkg.add("PhyloPlots")';
     julia -e 'Pkg.add("Documenter")';
     julia -e 'cd(Pkg.dir("PhyloNetworks")); include(joinpath("docs", "make.jl"))';
