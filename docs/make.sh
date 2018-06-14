f04640601c20b4207ba7e36f8b214f32de3fafa3
Paul Bastide <paul.bastide@gmail.com>
2018-06-14 18:46:13 +0200

    Maybe this syntax will work ?

--- docs/make.sh
+++ docs/make.sh
@@ -3,12 +3,6 @@
 
 set -ev
 
-# Install R
-if [ "$TRAVIS_OS_NAME" == "linux" ]; then sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E084DAB9; fi
-if [ "$TRAVIS_OS_NAME" == "linux" ]; then sudo add-apt-repository -y "deb http://cran.rstudio.com/bin/linux/ubuntu $(lsb_release -s -c)/"; fi
-if [ "$TRAVIS_OS_NAME" == "linux" ]; then sudo apt-get update -qq -y; fi
-if [ "$TRAVIS_OS_NAME" == "linux" ]; then sudo apt-get install git r-base r-base-dev r-recommended -y; fi
-
 # Build the doc
 if [ "$TRAVIS_OS_NAME" == "linux" ]; then
     julia -e 'Pkg.add("PhyloPlots")';
