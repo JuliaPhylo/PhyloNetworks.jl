a168e929860b495b090a06aeddbd948905476c78
Paul Bastide <paul.bastide@gmail.com>
2018-06-14 17:58:23 +0200

    Install R before PhyloPlots.

--- docs/make.sh
+++ docs/make.sh
@@ -4,7 +4,13 @@
 set -ev
 
 if [ "$TRAVIS_OS_NAME" == "linux" ]; then
-    julia -e 'Pkg.clone("https://github.com/cecileane/PhyloPlots.jl")';
+    ## Install R
+    sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E084DAB9;
+    sudo add-apt-repository -y "deb http://cran.rstudio.com/bin/linux/ubuntu $(lsb_release -s -c)/";
+    sudo apt-get update -qq -y;
+    sudo apt-get install git r-base r-base-dev r-recommended -y;
+    ## Build the doc 
+    julia -e 'Pkg.add("PhyloPlots")';
     julia -e 'Pkg.add("Documenter")';
     julia -e 'cd(Pkg.dir("PhyloNetworks")); include(joinpath("docs", "make.jl"))';
 fi
