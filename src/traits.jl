0b7ea0430179e5dea9cb014a961c1095bbb3897c
Cecile Ane <cecileane@users.noreply.github.com>
2018-10-09 07:08:08 -0500

    typos and net example in docstring

--- src/traits.jl
+++ src/traits.jl
@@ -285,10 +285,11 @@ This function computes the variance covariance matrix between the tips of the
 network, assuming a Brownian model of trait evolution (with unit variance).
 If optional argument `corr` is set to `true`, then the correlation matrix is returned instead.
 
-The function returns a `DataFrame` objects, with columns named as the tips of the network.
+The function returns a `DataFrame` object, with columns named by the tips of the network.
 
-It assumes that the network is in the pre-order. If checkPreorder is
-true (default), then it runs function `preoder` on the network beforehand.
+The calculation of the covariance matrix requires a pre-ordering of nodes to be fast.
+If `checkPreorder` is true (default), then `preorder` is run on the network beforehand.
+Otherwise, the network is assumed to be already in pre-order.
 
 This function internally calls [`sharedPathMatrix`](@ref), that computes the variance
 matrix between all the nodes of the network.
@@ -323,6 +324,20 @@ t3 0.14 0.14 1.10 0.0 0.00
 t5 0.00 0.00 0.00 1.6 0.90
 t1 0.00 0.00 0.00 0.9 1.08
 
+```
+
+The covariance can also be calculated on a network
+(for the model, see for Bastide et al. 2018)
+```jldoctest
+julia> net = readTopology("((t1:1.0,#H1:0.1::0.30):0.5,((t2:0.9)#H1:0.2::0.70,t3:1.1):0.4);");
+
+julia> C = vcv(net)
+3×3 DataFrames.DataFrame
+│ Row │ t1   │ t2    │ t3   │
+├─────┼──────┼───────┼──────┤
+│ 1   │ 1.5  │ 0.15  │ 0.0  │
+│ 2   │ 0.15 │ 1.248 │ 0.28 │
+│ 3   │ 0.0  │ 0.28  │ 1.5  │
 ```
 """
 function vcv(net::HybridNetwork;
