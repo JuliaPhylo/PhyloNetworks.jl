88bfe9e75b969dbb9a1e715a149d2edb24d15f17
Paul Bastide <paul.bastide@gmail.com>
2018-10-20 09:40:35 +0200

    Manually specify versions of mkdocs and mkdocs-material to avoid conflicts.

--- docs/make.jl
+++ docs/make.jl
@@ -2,8 +2,11 @@ using Documenter, PhyloNetworks
 
 makedocs()
 
+# Versions of `mkdocs` and `mkdocs-material` specified manually to avoid conflicts. 
+# See here: https://discourse.julialang.org/t/mkdocs-material-in-documenter/13764/3
+# To be kept in mind: those versions might evolve in the future.
 deploydocs(
-    deps   = Deps.pip("pygments", "mkdocs", "mkdocs-material", "python-markdown-math"),
+    deps   = Deps.pip("pygments", "mkdocs==0.17.5", "mkdocs-material==2.9.4", "python-markdown-math"),
     repo = "github.com/crsl4/PhyloNetworks.jl.git",
     julia  = "0.6",
     osname = "linux"
