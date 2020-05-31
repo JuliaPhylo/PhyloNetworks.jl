c9586f53fd665fb3e772298fe525c8dce9e0aac5
Gabe Hassler <gabriel.w.hassler@gmail.com>
2020-05-31 15:10:02 -0700

    fixing typo

--- src/traits.jl
+++ src/traits.jl
@@ -859,7 +859,7 @@ end
 
 Type for a BM process on a network. Fields are `mu` (expectation),
 `sigma2` (variance), `randomRoot` (whether the root is random, default to `false`),
-and `varRoot` (if the root is random, the variance of the root, defalut to `NaN`).
+and `varRoot` (if the root is random, the variance of the root, default to `NaN`).
 
 """
 mutable struct ParamsBM <: ParamsProcess
