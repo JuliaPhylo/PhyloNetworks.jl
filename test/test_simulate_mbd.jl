9326549205e73a19bdf57b0c5d863150abf0d296
Gabe Hassler <gabriel.w.hassler@gmail.com>
2020-06-05 15:42:09 -0700

    changing variable name to avoind conflice with other tests

--- test/test_simulate_mbd.jl
+++ test/test_simulate_mbd.jl
@@ -2,15 +2,15 @@
 
 ## Get a network and make it ultrametric
 net = readTopology("(((Ag:5,(#H1:1::0.056,((Ak:2,(E:1,#H2:1::0.004):1):1,(M:2)#H2:1::0.996):1):1):1,(((((Az:1,Ag2:1):1,As:2):1)#H1:1::0.944,Ap:4):1,Ar:5):1):1,(P:4,20:4):3,165:7);");
-P = 3
+trait_dim = 3
 
 
 @testset "Simulate data and check means and dimensions" begin
 
 ## Simulate a MBD
 Random.seed!(17920921); # fix the seed
-μ = randn(P)
-Σ = randn(P, P)
+μ = randn(trait_dim)
+Σ = randn(trait_dim, trait_dim)
 Σ = Σ * Σ' # needs to be positive definite
 pars = ParamsMultiBM(μ, Σ); # params of a MBD
 @test_logs show(devnull, pars)
@@ -23,8 +23,8 @@ traitsTips = sim[:Tips];
 traitsNodes = sim[:InternalNodes];
 
 # Check dimensions
-@test size(traitsTips) == (P, net.numTaxa)
-@test size(traitsNodes) == (P, net.numNodes - net.numTaxa)
+@test size(traitsTips) == (trait_dim, net.numTaxa)
+@test size(traitsNodes) == (trait_dim, net.numNodes - net.numTaxa)
 
 expectations = sim[:All, :Exp]
 
@@ -41,15 +41,15 @@ end
 
 ## Generate some values
 Random.seed!(18480224); # fix the seed
-μ = randn(P)
-Σ = randn(P, P)
+μ = randn(trait_dim)
+Σ = randn(trait_dim, trait_dim)
 Σ = Σ * Σ' # needs to be positive definite
 pars = ParamsMultiBM(μ, Σ); # params of a MBD
 
 N = 50000
 S = length(tipLabels(net));
-μ_sim = zeros(P, S)
-Σ_sim = zeros(P * S, P * S)
+μ_sim = zeros(trait_dim, S)
+Σ_sim = zeros(trait_dim * S, trait_dim * S)
 for i = 1:N
     tips = simulate(net, pars)[:Tips]
     μ_sim .+= tips
