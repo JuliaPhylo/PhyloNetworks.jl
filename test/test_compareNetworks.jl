bea38f388b4b83f1a4c8357589c322afc5e0ce8f
Cora Allen-Coleman <6508099+coraallencoleman@users.noreply.github.com>
2020-06-26 14:16:11 -0500

    removed test_broken, fixed wrong rooting in testing hardwiredClusterDistance_unrooted

--- test/test_compareNetworks.jl
+++ test/test_compareNetworks.jl
@@ -350,7 +350,7 @@ estminor = minorTreeAt(estnet, 1); # (5:1.0,(3:1.0,4:1.0):1.069,(6:1.0,(1:1.0,2:
 # so the hybrid edge was estimated correctly!!
 rootatnode!(trunet, -8)
 @test hardwiredClusterDistance(estnet, trunet, true) == 3
-@test_broken hardwiredClusterDistance(estnet, trunet, false) == 0
+@test hardwiredClusterDistance(estnet, trunet, false) == 0
 
 net5 = readTopology("(A,((B,#H1:::0.2),(((C,(E)#H2:::0.7),(#H2:::0.3,F)),(D)#H1:::0.8)));");
 tree = displayedTrees(net5, 0.0);
@@ -437,16 +437,13 @@ end # of testset: displayedTrees, hardwiredClusters, hardwiredClusterDistance, d
 @testset "testing hardwiredClusterDistance_unrooted" begin
 h0est = readTopology("(((2:0.01,1:0.01):0.033,(3:0.0154,4:0.0149):0.0186):0.0113,6:0.0742,5:0.0465);")
 truenet = readTopology("((((1,2),((3,4))#H1),(#H1,5)),6);")
+h1est = readTopology("(5:0.0,6:0.0,(((2:0.0)#H1:0.0::0.95,1:0.0):0.0,((4:0.0,3:0.0):0.0,#H1:0.0::0.05):0.0):0.0);")
+
 @test PhyloNetworks.hardwiredClusterDistance_unrooted(h0est, truenet) == 1
 @test PhyloNetworks.hardwiredClusterDistance_unrooted(truenet, h0est) == 1
 
-h1est = readTopology("(5:0.0,6:0.0,(((2:0.0)#H1:0.0::0.95,1:0.0):0.0,((4:0.0,3:0.0):0.0,#H1:0.0::0.05):0.0):0.0);")
-@test PhyloNetworks.hardwiredClusterDistance_unrooted(h1est, truenet) == 4
-@test PhyloNetworks.hardwiredClusterDistance_unrooted(truenet, h1est) == 4
-rootatnode!(h1est, "5")
-rootatnode!(truenet, "5")
-@test hardwiredClusterDistance(truenet, h1est, true) == 4
-@test hardwiredClusterDistance(h1est, truenet, true) == 4
+@test PhyloNetworks.hardwiredClusterDistance_unrooted(h1est, truenet) == 3
+@test PhyloNetworks.hardwiredClusterDistance_unrooted(truenet, h1est) == 3
 end
 
 @testset "testing hardwiredCluster! on single nodes" begin
