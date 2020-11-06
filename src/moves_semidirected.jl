151188d336209b2649ee49e56d2272bd39df73cd
Cora Allen-Coleman <6508099+coraallencoleman@users.noreply.github.com>
2020-11-06 14:35:03 -0600

    more detail on sNNIs

--- src/moves_semidirected.jl
+++ src/moves_semidirected.jl
@@ -370,23 +370,24 @@ end
     nni!(net::HybridNetwork, uv::Edge, nummove::UInt8,
          nohybridladder::Bool, no3cycle::Bool)
 
-Modify `net` with a nearest neighbor interchange (NNI) around edge `uv`.
-Return the information necessary to undo the NNI, or `nothing` if the move
-was not successful (such as if the resulting graph was not acyclic (not a DAG) or if
-the focus edge is adjacent to a polytomy). If the move fails, the network is not
-modified.
-`nummove` specifies which of the available NNIs is performed.
+Modify `net` with a semi-directed nearest neighbor interchange (sNNI) around
+edge `uv`. Return the information necessary to undo the sNNI, or `nothing` if \
+the move was not successful (such as if the resulting graph was not acyclic
+(not a DAG) or if the focus edge is adjacent to a polytomy). If the move fails,
+the network is not modified.
 
-rooted-NNI options according to Gambette et al. (2017), fig. 8:
+`nummove` specifies which of the available sNNIs is performed.
+
+Expanded from the rooted-NNI (rNNI) options in Gambette et al. (2017), fig. 8:
 * BB: 2 moves, both to BB, if directed edges. 8 moves if undirected.
 * RR: 2 moves, both to RR.
 * BR: 3 moves, 1 RB & 2 BRs, if directed. 6 moves if e is undirected.
 * RB: 4 moves, all 4 BRs.
-The extra options are due to assuming a semi-directed network, whereas
-Gambette et al (2017) describe options for rooted networks.
-On a semi-directed network, there might be a choice of how to direct
-the edges that may contain the root, e.g. choice of e=uv versus vu, and
-choice of labelling adjacent nodes as α/β (BB), or as α/γ (BR).
+Our extra sNNI options come from assuming a semi-directed network, in
+constrast to Gambette et al (2017)'s rooted networks. On a semi-directed network,
+there may be a choice in how to direct the edges that may contain the root:
+e.g. choice of e=uv versus vu and choice of labelling adjacent nodes as
+α/β (BB) or as α/γ (BR).
 
 `nohybridladder` = true prevents moves that would create a hybrid ladder in
 the network, that is, 2 consecutive hybrid nodes (one parent of the other).
