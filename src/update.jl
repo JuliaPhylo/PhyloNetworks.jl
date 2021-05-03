e6c4cecdd6396b2b605cfa634fd2d2f1e39c26e7
Tyler Chafin <tylerkchafin@gmail.com>
2021-05-03 12:14:41 -0600

    added functions to sample quartets and update Quartet.sampled

--- src/update.jl
+++ src/update.jl
@@ -448,13 +448,60 @@ function updateUninformativeQuartets!(quartets::Vector{Quartet}, atol::Float64)
 end
 updateUninformativeQuartets!(d::DataCF, atol::Float64) = updateUninformativeQuartets!(d.quartet, atol)
 
-function updateResetSampledQuartets!(quartets::Vector{Quartet}, toset::Bool)
+"""
+updateUninformativeQuartetsAll!(quartets::Vector{Quartet}, toset::Bool)
+
+Updates in-place all Quartet.uninformative values to the value provided in toset::Bool
+"""
+function updateUninformativeQuartetsAll!(quartets::Vector{Quartet}, toset::Bool)
+    Threads.@threads for q in quartets
+        q.uninformative=toset
+    end
+end
+updateUninformativeQuartetsAll!(quartets::Vector{Quartet}) = updateUninformativeQuartetsAll!(quartets, false)
+updateUninformativeQuartetsAll!(d::DataCF, toset::Bool) = updateUninformativeQuartetsAll!(d.quartet, toset)
+updateUninformativeQuartetsAll!(d::DataCF) = updateUninformativeQuartetsAll!(d.quartet, false)
+
+"""
+updateSampledQuartetsAll!(quartets::Vector{Quartet}, toset::Bool)
+
+Updates in-place all Quartet.sampled values where Quartet.uninformative=false 
+    to the value provided in toset::Bool
+"""
+function updateSampledQuartetsAll!(quartets::Vector{Quartet}, toset::Bool)
     Threads.@threads for q in quartets
         if !q.uninformative
             q.sampled=toset
         end
     end
 end
-updateResetSampledQuartets!(quartets::Vector{Quartet}) = updateResetSampledQuartets!(quartets, true)
-updateResetSampledQuartets!(d::DataCF, toset::Bool) = updateResetSampledQuartets!(d.quartet, true)
-updateResetSampledQuartets!(d::DataCF) = updateResetSampledQuartets!(d.quartet, true)
+updateSampledQuartetsAll!(quartets::Vector{Quartet}) = updateSampledQuartetsAll!(quartets, true)
+updateSampledQuartetsAll!(d::DataCF, toset::Bool) = updateSampledQuartetsAll!(d.quartet, true)
+updateSampledQuartetsAll!(d::DataCF) = updateSampledQuartetsAll!(d.quartet, true)
+
+"""
+updateSubsetQuartets!
+
+Function to randomly sample a proportion of quartets, and update in-place 
+    Quartet.sampled = true. Number of quartets to sample is defined 
+    as the supplied proportion (prop::Float64) * the number of informative 
+    quartets (i.e., where Quartet.uninformative==false)
+
+Output: None, quartets vector is modified in-place 
+"""
+function updateSubsetQuartets!(quartets::Vector{Quartet}, prop::Float64)
+    #reset Quartet.samples for all informative quartets
+    updateSampledQuartetsAll!(quartets, false)
+    
+    #get count of informative quartets
+    indices = [i for (i, q) in enumerate(quartets) if q.uninformative==false]
+    numQuartets = size(indices, 1)
+
+    #randomly sample numQuartets*propQuartets quartets
+    to_sample = Int(round(numQuartets*prop))
+    sampled = sample(indices, to_sample, replace=false)
+    for s in sampled 
+        quartets[s].sampled = true
+    end
+end
+updateSubsetQuartets!(d::DataCF, prop::Float64) = updateSubsetQuartets!(d.quartet, prop)
