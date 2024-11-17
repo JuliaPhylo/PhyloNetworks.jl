19dfcd0ce82a1b99a16885f37268d3b13c52c278
Cecile Ane <cecileane@users.noreply.github.com>
2024-11-17 11:25:33 -0500

    docstrings for types: defaults documentation, rm SNaQ comments

--- src/types.jl
+++ src/types.jl
@@ -31,24 +31,27 @@ Data structure for an edge and its various attributes. Most notably:
 - `ismajor` (boolean): whether the edge is the major path to the child node;
   `true` for tree edges, since a tree edge is the only path to its child node;
   normally true if `gamma>0.5`.
+- `containroot` (boolean): is the interior of this edge a valid rooting position?
+  That is, if we added a node on the edge, could we direct all edges away from
+  this new node to root the semidirected network and get a valid rooted network?
 
 and other fields, used very internally
 """
 mutable struct EdgeT{T<:ANode}
     number::Int
     length::Float64 # default 1.0
-    hybrid::Bool
-    y::Float64 # exp(-t), cannot set in constructor for congruence
-    z::Float64 # 1-y , cannot set in constructor for congruence
-    gamma::Float64 # set to 1.0 for tree edges, hybrid?gamma:1.0
+    hybrid::Bool   # default: false
+    y::Float64     # exp(-length), cannot set in constructor for congruence
+    z::Float64     # 1-y, cannot set in constructor for congruence
+    gamma::Float64 # default: 1.0 for tree edges, gamma for hybrid edges
     node::Vector{T}
-    ischild1::Bool # used for hybrid edges to set the direction (default true)
+    ischild1::Bool # default: true. used to fix the direction of hybrid edges.
     ismajor::Bool  # major edge treated as tree edge for network traversal
                    # true if gamma>.5, or if it is the original tree edge
-    inte1::Int     # sometimes used for cycle number if level-1
-    containroot::Bool # true if this edge may contain the root given the direction of hybrid edges
-    boole1::Bool # reusable booleans, for an edge
-    boole2::Bool
+    inte1::Int     # default: -1. used in SNaQ for cycle number if level-1
+    containroot::Bool # default: true
+    boole1::Bool   # default: true
+    boole2::Bool   # default: false
 end
 # outer constructors: ensure congruence among (length, y, z) and (gamma, hybrid, ismajor), and size(node)=2
 function EdgeT{T}(number::Int, length::Float64=1.0) where {T<:ANode}
@@ -108,31 +111,6 @@ Data structure for a node and its various attributes. Most notably:
   potentially more if the node has a polytomy
 - `hybrid` (boolean): whether the node is a hybrid node (with 2 or more parents)
   or a tree node (with a single parent)
-
-Other more internal attributes include:
-
-- `isBadDiamondI` -> `booln2` and `isBadDiamondII` -> `booln3` (booleans): whether the node is a
-  hybrid node where the reticulation forms a cycle of 4 nodes (diamond),
-  and where both parents of the hybrid nodes are connected to a leaf.
-  In a bad diamond of type I, the hybrid node itself is also connected
-  to a leaf but the common neighbor of the 2 hybrid's parents is not connected
-  to a leaf.
-  In a bad diamond of type II, the hybrid node has an internal node as child,
-  and the common neighbor of the 2 hybrid's parents is connected to a leaf.
-- `isBadTriangle` -> `booln6`, `isVeryBadTriangle` -> `booln5` and `isExtBadTriangle` -> `booln4` (booleans):
-  true if the reticulation forms a cycle of 3 nodes (triangle) and
-  depending on the number of leaves attached these 3 nodes. The triangle means
-  that the 2 parents of the hybrid node are directly related:
-  one is the child of the other. `isBadTriangle` is true if the triangle is
-  "good", as per Solís-Lemus & Ané (2016), that is, if all 3 nodes in the cycle
-  are not connected to any leaves (the reticulation is detectable from quartet
-  concordance factors, even though all branch lengths are not identifiable).
-  `isVeryBadTriangle` -> `booln5` is true if 2 (or all) of the 3 nodes are connected to a
-  leaf, in which case the reticulation is undetectable from unrooted gene tree
-  topologies (thus it's best to exclude these reticulations from a search).
-  `isBadTriangle` is true if exactly 1 of the 3 nodes is connected to a leaf.
-
-For details see Solís-Lemus & Ané (2016, doi:10.1371/journal.pgen.1005896)
 """
 mutable struct Node <: ANode
     number::Int
@@ -161,10 +139,22 @@ Node(number::Int, leaf::Bool, hybrid::Bool=false) = Node(number,leaf,hybrid,-1.,
 Node(number::Int, leaf::Bool, hybrid::Bool, edge::Vector{Edge}) = Node(number,leaf,hybrid,-1.,edge,any(e->e.hybrid,edge),false,false,false,false,false,-1.,nothing,-1,-1,"")
 Node(number::Int, leaf::Bool, hybrid::Bool,fvalue::Float64, edge::Vector{Edge}) = Node(number,leaf,hybrid,fvalue,edge,any(e->e.hybrid, edge),false,false,false,false,false,-1.,nothing,-1,-1,"")
 
-# partition type
+"""
+    Partition
+
+Data structure for a collection of edges. Used in SNaQ for groups of edges
+in the same cycle.
+Fields:
+
+- `cycle`: vector of `Int`
+- `edges`: vector of `Edge`
+
+Todo: use this to store the output of the blob decomposition, because
+biconnected components partition edges.
+"""
 mutable struct Partition
-    cycle::Vector{Int} #hybrid node number for cycle (or cycles)
-    edges::Vector{Edge} #edges in partition
+    cycle::Vector{Int} # hybrid node number for cycle (or cycles)
+    edges::Vector{Edge}
 end
 
 abstract type Network end
@@ -178,17 +168,21 @@ abstract type Network end
 Subtype of abstract `Network` type.
 Explicit network or tree with the following attributes:
 
-- numtaxa (taxa are tips, i.e. nodes attached to a single edge)
-- numnodes (total number of nodes: tips and internal nodes)
-- numedges
-- numhybrids (number of hybrid nodes)
-- edge (array of Edges)
-- node (array of Nodes)
-- rooti (index of root in vector 'node'. May be artificial, for printing and traversal purposes only.)
-- hybrid (array of Nodes: those are are hybrid nodes)
-- leaf (array of Nodes: those that are leaves)
-- fscore (score after fitting network to data, i.e. negative log pseudolik for SNaQ)
-- isrooted (true or false)
+- `numtaxa`: number of taxa, that is, number of are leaves (or tips).
+  Leaves are required to be attached to a single edge.
+- `numnodes`: total number of nodes: tips and internal nodes
+- `numedges`: total number of edges
+- `numhybrids`: total number of hybrid nodes
+- `edge`: vector of Edges
+- `node`: vector of Nodes
+- `rooti`: index of the root in vector 'node'. May be artificial in a semidirected
+  network, but is necessary for printing and traversal purposes.
+- `hybrid`: vector of Nodes: those are are hybrid nodes
+- `leaf`: vector of Nodes: those that are leaves
+- `fscore`: score after fitting network to data, i.e. parsimony score, or
+  multipe of the negative log pseudodeviance for SNaQ
+- `isrooted`: true or false
+- `partition`: vector of `Partition`
 """
 mutable struct HybridNetwork <: Network
     numtaxa::Int  # cannot set in constructor for congruence
