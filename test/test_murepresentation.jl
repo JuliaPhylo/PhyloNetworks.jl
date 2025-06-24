11d2dfbe02df5d57716a2c8f469194c196aa35a3
Cecile Ane <cecileane@users.noreply.github.com>
2025-06-24 16:35:26 -0500

    fix tests failing on pkgeval, Julia 1.13.0-DEV.771, from different order of dict keys

--- test/test_murepresentation.jl
+++ test/test_murepresentation.jl
@@ -22,13 +22,18 @@ end
 net1 = readnewick("((C,(B)#H1),(#H1,A));")
 @test_throws "labels should be sorted" PN.node_murepresentation(net1, ["D","A","B"])
 μ1 = (@test_logs (:warn, r"^leaf C") PN.node_murepresentation(net1, ["A","B","D"]))
-@test string(μ1) == """PhyloNetworks.NodeMuRepresentation
+s1 = string(μ1)
+@test startswith(s1, """PhyloNetworks.NodeMuRepresentation
 3 taxa in μ-vectors: ["A", "B", "D"]
 4 nodes, map node number => μ0 to hybrids and μ-vector to taxa:
-  -5 => μ0=1 μ=[1, 1, 0]
-  -3 => μ0=1 μ=[0, 1, 0]
-  -2 => μ0=2 μ=[1, 2, 0]
-  3 => μ0=1 μ=[0, 1, 0]"""
+""")
+for ss in [ # unpredictable order in a dictionary: changes with julia version
+"  -5 => μ0=1 μ=[1, 1, 0]",
+"  -3 => μ0=1 μ=[0, 1, 0]",
+"  -2 => μ0=2 μ=[1, 2, 0]",
+"  3 => μ0=1 μ=[0, 1, 0]",]
+  @test occursin(ss, s1)
+end
 
 # net1 as subnetwork: net2 = (net1, (D)), and degree-2 node in net2
 net2 = readnewick("(((A,(B)#H1),(#H1,C)),(D));")
@@ -41,17 +46,22 @@ labels = ["A","B","C","D"] # from net2, but sorted
     net1, net2; preorder=false, labels=["A","B"]))) == 2
 
 μ2o = PN.edge_murepresentation(net2; preorder=false)
-@test string(μ2o) == """PhyloNetworks.EdgeMuRepresentation
+s2o = string(μ2o)
+@test startswith(s2o, """PhyloNetworks.EdgeMuRepresentation
 4 taxa in μ-vectors: ["A", "B", "C", "D"]
 root μ-vector: μ0=2 μ=[1, 2, 1, 1]
 maps edge number => μ-entry:
 3 tree edges in the root component
-  4 => μ0=1 μ=[1, 1, 0, 0]; μ0=1 μ=[0, 1, 1, 1]
-  7 => μ0=1 μ=[0, 1, 1, 0]; μ0=1 μ=[1, 1, 0, 1]
-  8 => μ0=2 μ=[1, 2, 1, 0]; μ0=0 μ=[0, 0, 0, 1]
-2 edges in the directed part:
-  5 => incident; μ0=1 μ=[0, 1, 0, 0]
-  3 => incident; μ0=1 μ=[0, 1, 0, 0]"""
+""")
+for ss in [
+"  4 => μ0=1 μ=[1, 1, 0, 0]; μ0=1 μ=[0, 1, 1, 1]",
+"  7 => μ0=1 μ=[0, 1, 1, 0]; μ0=1 μ=[1, 1, 0, 1]",
+"  8 => μ0=2 μ=[1, 2, 1, 0]; μ0=0 μ=[0, 0, 0, 1]",
+"2 edges in the directed part:",
+"  5 => incident; μ0=1 μ=[0, 1, 0, 0]",
+"  3 => incident; μ0=1 μ=[0, 1, 0, 0]"]
+  @test occursin(ss, s2o)
+end
 
 deleteleaf!(net2, "D")
 μ2 = (@test_logs PN.node_murepresentation(net2, labels))
@@ -77,17 +87,22 @@ net2 = readnewick("(((((a,b))#H1,c)h2,#H1),d)R;")
 labs = ["a","b","c","d"]
 μ1  = PN.edge_murepresentation(net1; preorder=false)
 @test tiplabels(μ1) == labs # different order as in tiplabels(net1)
-@test string(μ1) == """
+s1 = string(μ1)
+@test startswith(s1, """
 PhyloNetworks.EdgeMuRepresentation
 4 taxa in μ-vectors: ["a", "b", "c", "d"]
 root μ-vector: μ0=2 μ=[2, 2, 1, 1]
 maps edge number => μ-entry:
 1 tree edges in the root component
-  6 => μ0=2 μ=[2, 2, 0, 0]; μ0=0 μ=[0, 0, 1, 1]
-3 edges in the directed part:
-  5 => incident; μ0=1 μ=[1, 1, 0, 0]
-  4 => incident; μ0=1 μ=[1, 1, 0, 0]
-  3 => tree; μ0=0 μ=[1, 1, 0, 0]"""
+""")
+for ss in [
+"  6 => μ0=2 μ=[2, 2, 0, 0]; μ0=0 μ=[0, 0, 1, 1]",
+"3 edges in the directed part:",
+"  5 => incident; μ0=1 μ=[1, 1, 0, 0]",
+"  4 => incident; μ0=1 μ=[1, 1, 0, 0]",
+"  3 => tree; μ0=0 μ=[1, 1, 0, 0]"]
+  @test occursin(ss, s1)
+end
 net1r = deepcopy(net1); rootatnode!(net1r, 4);
 μ1r = PN.edge_murepresentation(net1r, labs)
 @test string(μ1r.mumap_rootcomp[6]) == "(μ0=0 μ=[0, 0, 1, 1], μ0=2 μ=[2, 2, 0, 0])"
